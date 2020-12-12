d8b3e9c9c64ffdae25030b1751caa6a866dce2d7
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2020-12-12 18:53:22 -0300

    Configuraçao adicionada ao settings.py para salvar as imagens em media/

--- src/marketplace/settings.py
+++ src/marketplace/settings.py
@@ -130,6 +130,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_URL = '/images/' # URL das imgs
+MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images') # Pasta onde serão salvas imgs upadas
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
